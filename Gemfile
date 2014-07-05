eed0d92ec1248b4725b9dee668b355d247d1f116
Florian Frank <flori@ping.de>
2014-07-05 23:09:01 +0200

    Adds codeclimate code coverage

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@
 source 'https://rubygems.org'
 
 gemspec
+
+gem "codeclimate-test-reporter", group: :test, require: nil
--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -4,6 +4,10 @@ if ENV['START_SIMPLECOV'].to_i == 1
     add_filter "#{File.basename(File.dirname(__FILE__))}/"
   end
 end
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 require 'minitest/unit'
 require 'test/unit'
 require 'term/ansicolor'
