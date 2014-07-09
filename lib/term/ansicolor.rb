5849d80957666c8d415b85fff16c3074a565c849
Florian Frank <flori@ping.de>
2014-07-09 21:08:37 +0200

    Refactors: split color into methods and functions

--- lib/term/ansicolor.rb
+++ lib/term/ansicolor.rb
@@ -59,49 +59,106 @@ module Term
     # decoloring strings.
     COLORED_REGEXP = /\e\[(?:(?:[349]|10)[0-7]|[0-9]|[34]8;5;\d{1,3})?m/
 
-    # Returns an uncolored version of the string, that is all
-    # ANSI-Attributes are stripped from the string.
-    def decolor(string = nil) # :yields:
-      new_string_like(
+    def check_function_arguments(string, &block)
+      if block_given? || string
+        unless block_given? ^ string
+          exception = ArgumentError.new(
+            'passing of either a block or an argument is required'
+          )
+          exception.set_backtrace caller
+          raise exception
+        end
+      end
+    end
+
+    def decolor_function(string, &block)
+      check_function_arguments(string, &block)
+      new_string_like(string,
         if block_given?
           yield.to_str.gsub(COLORED_REGEXP, '')
         elsif string.respond_to?(:to_str)
           string.to_str.gsub(COLORED_REGEXP, '')
-        elsif respond_to?(:to_str)
-          to_str.gsub(COLORED_REGEXP, '')
         else
-          ''
+          string.to_s.gsub(COLORED_REGEXP, '')
         end
       )
+    end
+
+    def decolor_method()
+      new_string_like(self,
+        if respond_to?(:to_str)
+          to_str.gsub(COLORED_REGEXP, '')
+        else
+          to_s.gsub(COLORED_REGEXP, '')
+        end
+      )
+    end
+
+    # Returns an uncolored version of the string, that is all
+    # ANSI-Attributes are stripped from the string.
+    def decolor(string = nil, &block)
+      if string || block_given?
+        decolor_function(string, &block)
+      else
+        decolor_method
+      end
     end
 
     alias uncolor decolor
 
     alias uncolored decolor
 
-    def new_string_like(string = '')
-      respond_to?(:to_str) ? self.class.new(string) : string
+    def new_string_like(other_string, string = '')
+      if other_string.respond_to?(:to_str) &&
+        [ 1, -1, -2 ].include?(other_string.class.instance_method(:initialize).arity)
+      then
+        other_string.class.new(string)
+      else
+        string.dup
+      end
     end
     private :new_string_like
 
-    # Return +string+ or the result string of the given +block+ colored with
-    # color +name+. If string isn't a string only the escape sequence to switch
-    # on the color +name+ is returned.
-    def color(name, string = nil, &block)
+    def color_method(name)
       attribute = Attribute[name] or raise ArgumentError, "unknown attribute #{name.inspect}"
-      result = new_string_like
+      result = new_string_like(self)
+      result << "\e[#{attribute.code}m" if Term::ANSIColor.coloring?
+      if respond_to?(:to_str)
+        result << to_str
+      end
+      result << "\e[0m" if ::Term::ANSIColor.coloring?
+      result
+    end
+    private :color_method
+
+    def color_function(name, string = nil, &block)
+      attribute = Attribute[name] or raise ArgumentError, "unknown attribute #{name.inspect}"
+      check_function_arguments(string, &block)
+      result = new_string_like(string)
       result << "\e[#{attribute.code}m" if Term::ANSIColor.coloring?
       if block_given?
         result << yield
       elsif string.respond_to?(:to_str)
         result << string.to_str
-      elsif respond_to?(:to_str)
-        result << to_str
+      elsif string
+        result << string.to_s
       else
         return result # only switch on
       end
       result << "\e[0m" if ::Term::ANSIColor.coloring?
       result
+    end
+    private :color_function
+
+    # Return +string+ or the result string of the given +block+ colored with
+    # color +name+. If string isn't a string only the escape sequence to switch
+    # on the color +name+ is returned.
+    def color(name, string = nil, &block)
+      if string || block_given? || !respond_to?(:to_str)
+        color_function(name, string, &block)
+      else
+        color_method(name)
+      end
     end
 
     def on_color(name, string = nil, &block)
