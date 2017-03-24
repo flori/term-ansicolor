734281714ff67b0ef2127948f6b444fa5ea038f9
Florian Frank <flori@ping.de>
2017-03-24 17:12:11 +0100

    Remove codeclimate stuff

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -4,10 +4,6 @@ if ENV['START_SIMPLECOV'].to_i == 1
     add_filter "#{File.basename(File.dirname(__FILE__))}/"
   end
 end
-if RUBY_VERSION >= '1.9' && ENV['CODECLIMATE_REPO_TOKEN']
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
 require 'minitest/unit'
 require 'test/unit'
 require 'term/ansicolor'
