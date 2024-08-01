89e6ea719c93fa70feca7bc4dccc5623cb8b590d
Florian Frank <flori@ping.de>
2024-08-01 18:01:33 +0200

    Bump version to 1.11.0

--- lib/term/ansicolor/version.rb
+++ lib/term/ansicolor/version.rb
@@ -1,6 +1,6 @@
 module Term::ANSIColor
   # Term::ANSIColor version
-  VERSION         = '1.10.4'
+  VERSION         = '1.11.0'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
